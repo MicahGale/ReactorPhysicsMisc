fcf383ba2ec671cbd9508012979187ba1da43159
Micah Gale <mgale@mit.edu>
2018-02-19 15:34:38 -0500

    Corrected lethergy binning thing

--- PS1/formLogBounds.m
+++ PS1/formLogBounds.m
@@ -1,9 +1,9 @@
 
 function edges=formLogBounds(lower, Upper, bins)
     upperEnd=log(Upper/lower);
-    size=(upperEnd-1)/bins;
-    edges=[lower];
-    for i=upperEnd:-size:1
+    size=(upperEnd)/bins;
+    edges=[];
+    for i=upperEnd:-size:0
         edge=Upper/exp(i);
         edges=[edges;edge];
     end
