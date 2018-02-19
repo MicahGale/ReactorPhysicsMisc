3265d9515fe804ddc45eb7315d52edef80799e49
Micah Gale <mgale@mit.edu>
2018-02-18 23:09:45 -0500

    Implemented SLBW at 0K math

--- lib/materials.h
+++ lib/materials.h
@@ -1,3 +1,4 @@
+#include <cmath>           //math things like sqrt
 #include "../lib/event.h"    //event type
 /**
  * Holds the material data for an isotope
@@ -12,17 +13,29 @@ class material {
 	double sigPot; //potential scattering
 	double N;       //number density
 	float alpha;   //cache the alpha term
-	int A;	
-	std::vector<double> r;
+	int A;
+	//resonance data	
 	std::vector<double> E0; //resonance energies
 	std::vector<double> GG; //gamma for (n,gamma)
 	std::vector<double> GN; //gamma for (n,n)
+	double T;
+
+	//calculated resonance info
 	std::vector<double> G;  //gamma total
+	std::vector<double> r; 
+	std::vector<double> q;
+	std::vector<double> SLBW_squiggle;
 	public:
+	/**
+	*Boltzmann Constant taken from: <https://en.wikipedia.org/wiki/Boltzmann_constant>
+	*In units of eV/k
+	*/
+	const double BOLTZ_K=8.6173303e-5;
 	/**
 	 *Constructor 
 	 *
-	 */	
+	 */
+
 	material (std::string name, int A, double N, double sigPot) {
 		int top, bottom;
 		this->name=name;
@@ -40,20 +53,56 @@ class material {
 		this->GN=GN;
 		this->G.reserve(E0.size());
 		this->r.reserve(E0.size()); //initialize vectors before using them
+		this->q.reserve(E0.size());
 		for(int i=0; i<GG.size(); i++) {
 			this->G[i]=GG[i]+GN[i];
 			this->r[i]=2603911/E0[i]*(this->A+1)/this->A; //calculate r
+			this->q[i]=2*sqrt(r[i]*sigPot);     //2sqrt(r*sig_pot)
 		}
 	}
 	double getMacroSigP(double E, double T) {
 		return this->sigPot*this->N;
 	}
-
+	/**
+	 *Calculates the sigma_s with the SLBW.
+	 * excludes the potential term.
+	 *
+	 * @param E the energy to evaluate
+	 * @param T the temperature to evaluate
+	 * @return the resonant contribution to sigma_s in barns
+	 */
 	double getMicroSigSRes(double E, double T) {
-		return 0;
-	} 
+		double sum=0;
+		double gammaTerm, phi,chi;
+
+		for(int i=0; i<E0.size(); i++) {
+			phi=this->get_SLBW_phi(E,T);
+			chi=this->get_SLBW_chi(E,T);
+			gammaTerm=GN[i]*GN[i]/G[i]*G[i];
+			sum+=gammaTerm*(r[i]*phi+G[i]/GN[i]*q[i]*chi);
+			//calculates the SLBW resonance
+		}
+		return sum;
+	}
+	double getMicroSigS(double E, double T) {
+		return this->getMicroSigSRes(E,T)+sigPot;
+	}
+       /**
+	*Calculates the absorption cross-section using SLBW.
+	*
+	*@param E energy
+	*@param T temperature
+	*@return the full absorption cross-section
+	*/	
 	double getMicroSigA(double E, double T) {
-		return 0;
+		double sum=0; //the sum of all resonances
+		double gammaTerm, phi;
+		for(int i=0; i<E0.size();i++) {
+			phi=this->get_SLBW_phi(E,T);
+			gammaTerm=GN[i]*GG[i]/(G[i]*G[i]); // Gn/G* Gg/G
+			sum+=gammaTerm*sqrt(E0[i]/E)*r[i]*phi; //calculate SLBW sig a
+		}
+		return sum;
 	}
 
 	double getMacroSigS(double E, double T) {
@@ -88,5 +137,45 @@ class material {
 		}
 		return output;	
 	}
-
+	//################################SLBW Section#################################
+	private:
+	double get_SLBW_x(double E, int resPointer) {
+		if(resPointer>E0.size()-1) //if out of bounds have none of that
+			return 0;
+		return 2*(E-E0[resPointer])/G[resPointer];  //2(E-E0)/Gamma
+	}
+	/**
+	*prepares squiggle for doppler broadening
+	*/
+	double get_SLBW_temperature(double T) {
+		SLBW_squiggle.reserve(E0.size()); //allocate that space
+		for(int i=0; i<E0.size();i++) {
+			if(T>1) 
+				SLBW_squiggle[i]=G[i]*sqrt(A/(4*BOLTZ_K*T*E0[i]));
+			else
+				SLBW_squiggle[i]=1;
+		}
+		this->T=T;	
+	}  
+	double get_SLBW_phi(double E, int resPointer) {
+		double x;
+		x=this->get_SLBW_x(E,resPointer);
+		if(abs(T)<0.01) {  //if its really small then it's 0K.
+			return 1/(1+x*x); //1\(1+x^2)
+		} else {
+			//screw doppler broadening
+			return 0;
+		}
+	}
+	
+	double get_SLBW_chi(double E, int resPointer) {
+		double x;
+		x=this->get_SLBW_x(E, resPointer);
+		
+		if(abs(T)<0.01) {  //do the 0k!!!
+			return x/(1+x*x);  //  x/(1+x^2)
+		} else {
+			return 0;
+		}
+	}
 };
