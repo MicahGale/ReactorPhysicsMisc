1e0998a394e37807715fe5750080c7a7b87178c9
Micah Gale <mgale@mit.edu>
2018-03-04 18:16:29 -0500

    Implemented uniform x sources

--- lib/sources.h
+++ lib/sources.h
@@ -2,6 +2,18 @@
 class source {
 	public:
 		virtual event getNextNeutron();
+		vec getIsoDir() {
+			double mu, phi,x,y,z,xy;
+
+			mu=2*getSquiggle()-1;
+			phi=2*M_PI*getSquiggle();
+			z=mu; 
+			xy=std::pow(1-mu*mu,0.5); //project onto the xy plane
+			x=std::cos(phi)*xy;
+			y=std::sin(phi)*xy;
+			std::vector<double> ret {x,y,z};
+			return vec(ret); //return the isotropic source
+		}
 };
 
 class isoPntSrc: public source {
@@ -11,6 +23,30 @@ class isoPntSrc: public source {
 	public:
 		event getNextNeutron() {
 			//TODO implement
+		}
+
+};
+class isoXLineSrc: public source {
+	private:
+		double xMax;
+		double xMin;
+		double E;
+
+	public:
+		event getNextNeutron() {
+			vec point, dir;
+			double x,y,z;
+
+			dir=this->getIsoDir(); //find the direction
+			x=(xMax-xMin)*getSquiggle()+xMin; //go within the x range
+			y=rand()-RAND_MAX/2;  //just anywhere in space 
+			z=rand()-RAND_MAX/2;
+		        std::vector<double> ret {x,y,z};
+			point=vec(ret);
+			return event(this->sampleE(),event::NO_EVENT,point,dir);	
+		}
+		double sampleE() {
+			return E;
 		}
 
 };
