065f1377531f1a5ddf81617589505b28c2220a95
Micah Gale <mgale@mit.edu>
2018-03-05 00:19:23 -0500

    fleshed out tally skeleton

--- lib/tallies.h
+++ lib/tallies.h
@@ -1,6 +1,30 @@
 
 class tally {
 	public: 
-		tally() {}
-		virtual void doTally(const event& end, const vec& start);
+	//	tally() {}
+		virtual void doTally(const event& end, const vec& start)=0;
+};
+
+class collideTally: public tally {
+	private:
+
+	public:
+		collideTally() {
+
+		}
+		void doTally(const event& end, const vec& start) {
+
+		}
+};
+
+class trackTally: public tally {
+	private:
+
+	public:
+		trackTally() {
+
+		}
+		void doTally(const event& end, const vec& start) {
+
+		}
 };
