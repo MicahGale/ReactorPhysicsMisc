14afb6e1a64258090b13ba067bf5235115f1365a
Micah Gale <mgale@mit.edu>
2018-03-13 17:44:23 -0400

    Implemented rocketonium. Such... hack... codegit status

---
+++ PS3/ps3.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h> 
+#include <iostream>
+#include <fstream>
+#include <string>  
+#include <array> 
+#include <vector> 
+#include <sstream> 
+#include <tr1/memory> //shared_ptr library
+#include <stdlib.h>                           
+#include <cmath>
+
+class rocketonium  {
+	private:	
+		 std::vector<double> peaks ={1e3, 2e3 };
+		 double sigPot             =50;
+	public:
+		rocketonium(){}
+		/**
+		*Hard coding in data just seems dirty.
+		*/
+		double getPeak(double diff) {
+			if(diff>=-0.3 && diff<-0.1) {
+				return 5e3*diff+1.5e3;
+			} else if (diff>=-0.1 && diff<0) {
+				return 20e3*diff+ 4e3;
+			} else if(diff>=0&& diff <0.1) {
+				return -20e3*diff+4e3;
+			} else if( diff>=0.1 && diff<=0.3) {
+				return -5e3*diff+1.5e3;
+			}
+
+		}
+		double getSigS(double E) {
+			return sigPot;
+		}
+		double getSigA(double E) {
+			
+			for(double peak: peaks) {
+				if(fabs(peak-E)<0.3) {
+					return getPeak(E-peak); //treat as identical peaks
+				}
+			}
+			return 0;	
+		} 
+		double getSigT(double E) {
+			return getSigA(E)+getSigS(E);
+
+		}
+
+};
+
+double Q1C() {
+	rocketonium rocket; //init that stuff
+}
+
+int main() {
+	
+	return 1;
+}
