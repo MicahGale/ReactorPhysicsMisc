f39dba6067d1a81035ef0b1d7dead4a5a47c67ae
Micah Gale <mgale@mit.edu>
2018-03-13 22:38:50 -0400

    Completed Q1C. Also ignored tex outputs

--- PS3/ps3.cpp
+++ PS3/ps3.cpp
@@ -9,6 +9,9 @@
 #include <stdlib.h>                           
 #include <cmath>
 
+using namespace std;
+
+double trapInt(vector<double>& vals,double stepSize);
 class rocketonium  {
 	private:	
 		 std::vector<double> peaks ={1e3, 2e3 };
@@ -46,11 +49,91 @@ class rocketonium  {
 			return getSigA(E)+getSigS(E);
 
 		}
+		double NR_RI_intGrand(double E, double sigD) {
+			return getSigA(E)/(E*(getSigT(E)+sigD));
+		}
+		double get_NR_flux( double E, double sigD) {
+			return (sigPot+sigD)/(E*(getSigT(E)+sigD));
+		}
+
+		double get_WR_flux(double E, double sigD) {
+			return sigD/(E*(getSigA(E)+sigD));
+		}
+		double WR_RI_intGrand(double E, double sigD) {
+			return getSigA(E)/(E*(getSigA(E)+sigD));
+		}
+
+		double collapseXS_NR(double lowE, double upE,double sigD, int bins) {
+			vector<double> RI, flux;
+			double stepSize;
+
+			stepSize=(upE-lowE)/bins;
+			for(double E=lowE;E<=upE;E+=stepSize) {
+				RI.push_back(NR_RI_intGrand(E,sigD));
+				flux.push_back(get_NR_flux(E,sigD));
+			}
+			return (sigPot+sigD)*trapInt(RI,stepSize)/trapInt(flux,stepSize);
+			//do the collapse integral
+		}
+                double collapseXS_WR(double lowE, double upE,double sigD, int bins) {
+                        vector<double> RI, flux;
+                        double stepSize;
+
+                        stepSize=(upE-lowE)/bins;
+                        for(double E=lowE;E<=upE;E+=stepSize) {
+                                RI.push_back(WR_RI_intGrand(E,sigD));
+                                flux.push_back(get_WR_flux(E,sigD));
+                        }
+                        return (sigD)*trapInt(RI,stepSize)/trapInt(flux,stepSize);
+                        //do the collapse integral
+                }
+
 
 };
+double trapInt(vector<double> & vals, double stepSize) {
+	double sum;
+	int multiplier;
 
+	sum=0;
+	for(int i=0;i<vals.size();i++) {
+		if(i==0||i==vals.size()-1) {
+			multiplier=1;
+		} else {
+			multiplier=2;
+		}
+		sum+=multiplier*vals[i]*stepSize;
+	
+	}
+	return sum;
+
+}
 double Q1C() {
 	rocketonium rocket; //init that stuff
+	double sigD, C_nat_sig_t,lowE,upE;
+	vector<double> dilution;
+	int bins;
+	ofstream out;
+
+	//define problem constants
+	C_nat_sig_t=4.75; //eyeball it from ENDF-B/VII.1
+	dilution= {10,100,1e3};
+	lowE=1.0;
+	upE=5e3;
+	bins=500e3;
+
+	//file stuff
+	out.open("Q1Cxs.tex",ios::trunc);  //opens it
+	out<<"\\begin{tabular}{|c|c|c|}"<<endl;
+	out<<"\\textbf{C/R ratio} & NR $\\sigma_a$& WR$\\sigma_a$ \\\\\\hline"<<endl;
+
+	for(double dilut: dilution) {
+		sigD=dilut*C_nat_sig_t; //calculate the dilution cross-section
+            out<<dilut<< " & "<<rocket.collapseXS_NR(lowE,upE,sigD,bins)
+		    <<" & " <<rocket.collapseXS_WR(lowE,upE,sigD,bins)<<"\\\\\\hline"<<endl;
+
+	}
+	out<<"\\end{tabular}"<<endl;
+	out.close();
 }
 void testRocket() {
 	rocketonium rocket;
@@ -66,6 +149,7 @@ void testRocket() {
 
 
 }
-int main() {	
+int main() {
+	Q1C();	
 	return 1;
 }
